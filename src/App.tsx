08d306f549935afd0cefb994341614fcffd4bc1a
Carlos Eduardo Guimarães <eduardocarlos456@gmail.com>
2022-08-27 11:17:05 -0300

    Create basics components Header and Layout;

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
+import { Box, createTheme, ThemeProvider } from "@mui/material";
+import { Header } from "./components/Header";
+import { Layout } from "./components/Layout";
+
+const theme = createTheme({});
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  return <ThemeProvider theme={theme}>
+    <Box 
+      component="main"
+      sx={{
+      height: "100vh",
+        // backgroundColor: "#000"
+      }}
+    >
+      <Header />
+      <Layout><h1>Ola mundo</h1></Layout>
+    </Box>
+  </ThemeProvider>;
 }
 
 export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import { Box, Container } from '@mui/material'
+import React from 'react'
+
+export function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <Box>
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 , bg: "white"}}>
+                {children}
+            </Container>
+        </Box>
+    )
+}
