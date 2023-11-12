ea9c38d04ab7ab7b17df4dd8fd5c5eccdaf144b9
Carlos Eduardo Guimarães <eduardocarlos456@gmail.com>
2023-11-12 17:10:47 -0300

    - changed baseurl;

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = 'http://localhost:8000/api';
+const baseUrl = 'https://codeflixadm.bsite.net';
 
 export const apiSlice = createApi({
     reducerPath: "api",
